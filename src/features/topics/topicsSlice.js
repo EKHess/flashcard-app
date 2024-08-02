1c578bbbcb5810ea92d7eeb3c2d1a305e1c9f4a7
Eric Hess <110352704+EKHess@users.noreply.github.com>
2024-08-01 19:35:10 -0600

    Created topicsSlice.js

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -2,5 +2,28 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     topics: {}
-};
+}
 
+export const topicsSlice = createSlice({
+    name: 'topics',
+    initialState,
+    reducers: {
+        addTopic: (state, action) => {
+            state.topics[action.payload.id] = {
+                id: action.payload.id,
+                name: action.payload.name,
+                icon: action.payload.icon,
+                // add an empty array of quizzes associated with topic when that topic is created
+                quizIds: [],  
+            }
+        }
+    }
+})
+
+export const topicsSelector = (initialState) => {
+    initialState.topics
+}
+
+export const { addTopic } = topicsSlice.actions;
+
+export default topicsSlice.reducer;
