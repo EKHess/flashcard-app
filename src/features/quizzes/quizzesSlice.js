e318cd39f11c7cb990c1074817b3fdf0a677f447
Eric Hess <110352704+EKHess@users.noreply.github.com>
2024-08-04 14:32:03 -0600

    Created quizzesSlice and added it to the store.

---
+++ src/features/quizzes/quizzesSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    quizzes: {},
+}
+
+const quizzesSlice = createSlice({
+    name: 'quizzes',
+    initialState: initialState,
+    reducers: {
+        addQuiz: (state, action) => {
+            const { id, name, topicId, cardIds } = action.payload;
+            state.quizzes[id] = {
+                id: id,
+                name: name,
+                topicId: topicId,
+                cardIds: cardIds,
+            }
+        }
+    }
+})
+
+export const quizzesSelector = state => state.quizzes.quizzes;
+
+export const { addQuiz } = quizzesSlice.actions;
+
+export default quizzesSlice;
